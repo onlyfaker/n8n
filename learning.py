6f22ce5c176a8d61603ff5e14cd05027e5c0cb2f
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-06-25 17:34:50 -0400

    Update learning.py

--- learning.py
+++ learning.py
@@ -1,6 +1,6 @@
 #TODO - DAY1: learning n8n(22.6.2025)
 #TODO - DAY2: learning n8n(23.6.2025)
-#TODO - DAY3: learning n8n(24.6.2025)
-#TODO - DAY4: learning n8n(25.6.2025)
+#TODO - DAY3: building an AI agent in n8n(24.6.2025)
+#TODO - DAY4: building an AI agent in n8n(25.6.2025)
 
 
